cd60065127ee2069455418c7463966ddccb17196
FIUBA-MeMo1 <guvillagr@fi.uba.ar>
2024-08-30 03:55:18 +0000

    Modificación del script para almancenar reportes de la prueba

--- MeMo1-Ejercicio1/app/src/test/java/memo1/ejercicio1/CucumberTest.java
+++ MeMo1-Ejercicio1/app/src/test/java/memo1/ejercicio1/CucumberTest.java
@@ -5,5 +5,13 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/memo1/ejercicio1")
+@CucumberOptions(
+  features = "src/test/resources/memo1/ejercicio1",  // Ruta de tus archivos .feature
+  plugin = {
+    "pretty",  // Para ver los resultados en consola de manera legible
+    "html:build/reports/cucumber/cucumber-html-report.html",  // Reporte en HTML
+    "json:build/reports/cucumber/cucumber-report.json",  // Reporte en JSON
+    "junit:build/reports/cucumber/cucumber-results.xml"  // Reporte en formato JUnit (XML)
+  }
+)
 public class CucumberTest {}
